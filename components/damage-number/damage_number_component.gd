a6c693ee8667e954e391e5a3b00c4cc45f65126d
Tamicktom <rickpokemon38@gmail.com>
2024-12-30 03:35:26 -0300

    better typo

--- components/damage-number/damage_number_component.gd
+++ components/damage-number/damage_number_component.gd
@@ -4,12 +4,12 @@ class_name DamageNumberComponent;
 
 @onready var damage_label = preload("res://components/damage-number/DamageNumberLabelComponent.tscn");
 
-func show_damage_number(attack_damage: int) -> void:
-	# grab all the children
-	var children: Array[Node] = get_children();
+# Tipe children to only be DamageNumberPositionComponent
+@export var damage_number_position_component: Array[DamageNumberPositionComponent] = [];
 
-	# select a random child
-	var selected_child: Node = children[randi() % children.size()];
+func show_damage_number(attack_damage: int) -> void:
+	# select a random damage number position component
+	var selected_child: DamageNumberPositionComponent = damage_number_position_component[randi() % damage_number_position_component.size()];
 
 	if selected_child is DamageNumberPositionComponent:
 		var child_position: Vector2 = selected_child.position;
