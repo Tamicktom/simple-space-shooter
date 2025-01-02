6d979936ddaad00c8ca3c8fb83af1a06996ff5f6
Tamicktom <rickpokemon38@gmail.com>
2025-01-01 21:30:10 -0300

    update player movement for mouse or keyboard

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,12 +1,15 @@
-extends CharacterBody2D
+extends CharacterBody2D;
 
 class_name Player;
 
 @onready var parent: Level = get_parent();
-@export var speed: float = 1000.0;
+@export var speed: float = 500.0;
 # This is the distance from the player to the mouse to start moving the player
 @export var mouse_player_distance_threshold: float = 8.0; 
 var can_shoot: bool = true;
+
+enum ControlType {MOUSE, KEYBOARD}
+@export var control_type: ControlType = ControlType.KEYBOARD;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,32 +21,30 @@ func _process(delta: float) -> void:
 		$Timers/AttackCooldown.start();
 
 func move_player(_delta: float) -> void:
-	# Get the input direction and the current velocity
-	# var direction = Input.get_vector("left", "right", "up", "down");
-	# velocity = direction * speed;
-	# this is a spaceship shooter game, so we need to make the player follow the mouse
-	var mouse_position: Vector2 = get_global_mouse_position();
+	if control_type == ControlType.MOUSE:
+		move_player_by_mouse(_delta);
+	elif control_type == ControlType.KEYBOARD:
+		move_player_by_keyboard(_delta);
+	move_and_slide();
 
-	# convert the mouse position values to int
+func move_player_by_keyboard(_delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down");
+	velocity = direction * speed;
+
+func move_player_by_mouse(_delta: float) -> void:
+	var mouse_position: Vector2 = get_global_mouse_position();
 	var direction: Vector2 = (mouse_position - position).normalized();
 
 	var differenceX: float = mouse_position.x - position.x;
 	var differenceY: float = mouse_position.y - position.y;
 
-	var can_move_x: bool = differenceX > mouse_player_distance_threshold or differenceX < -mouse_player_distance_threshold;
-	var can_move_y: bool = differenceY > mouse_player_distance_threshold or differenceY < -mouse_player_distance_threshold;
+	var can_move_x: bool = differenceX > mouse_player_distance_threshold || differenceX < -mouse_player_distance_threshold;
+	var can_move_y: bool = differenceY > mouse_player_distance_threshold || differenceY < -mouse_player_distance_threshold;
 
 	if can_move_x or can_move_y:
 		velocity = direction * speed;
 	else:
 		velocity = Vector2.ZERO;
-
-	print("Global Mouse position: ", mouse_position);
-	print("Local Mouse position: ", get_local_mouse_position());
-	print("Player position: ", position);
-	print("Viewport size: ", get_viewport_rect().size);
-	# Move the player
-	move_and_slide();
 
 func shoot():
 	# if father is a Level
