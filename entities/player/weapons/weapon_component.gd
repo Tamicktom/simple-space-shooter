c530475b23b125796fd656fa2d3180a83ba5a5ed
Tamicktom <rickpokemon38@gmail.com>
2025-01-02 03:58:09 -0300

    add fire sound

--- entities/player/weapons/weapon_component.gd
+++ entities/player/weapons/weapon_component.gd
@@ -3,6 +3,7 @@ extends Node2D;
 class_name WeaponComponent;
 
 @onready var fire_cooldown: Timer = $Cooldowns/Fire;
+@onready var fire_sound: AudioStreamPlayer = $FireSound;
 
 #search for a Level node in the parent hierarchy
 @onready var level: Level = get_tree().get_first_node_in_group("Level");
@@ -10,6 +11,7 @@ class_name WeaponComponent;
 @export var fire_rate: float = 0.5;
 @export var fire_positions: Array[Marker2D] = [];
 @export var multiple_shots: bool = false;
+@export var fire_sound_path: AudioStream = load("res://sound/effects/13.mp3");
 
 var can_shoot: bool = true;
 
@@ -31,6 +33,9 @@ func shoot() -> void:
 	level.player_shoot(get_fire_position());
 	can_shoot = false;
 	fire_cooldown.start();
+	fire_sound.volume_db = ConfigOptions.get_property(ConfigOptions.VolumeProperty.SOUND);
+	fire_sound.stream = fire_sound_path;
+	fire_sound.play();
 
 func on_fire_timeout() -> void:
 	can_shoot = true;
--- globals/config_options.gd
+++ globals/config_options.gd
@@ -2,21 +2,22 @@ extends Node;
 
 var db_min: float = -80.0;
 
+var global_volume_percentage: float = 0.8;
+
 enum VolumeProperty {
-    GLOBAL,
-    MUSIC,
-    SOUND
+	MUSIC,
+	SOUND,
+	VOICE,
 }
 
 var audio_percentages: Dictionary = {
-	VolumeProperty.GLOBAL: 0.8,
 	VolumeProperty.MUSIC: 1.0,
-	VolumeProperty.SOUND: 1.0
+	VolumeProperty.SOUND: 1.0,
+	VolumeProperty.VOICE: 1.0,
 }
 
 func parse_percentage_to_db(percentage: float) -> float:
-	var db: float = (percentage * 100) + db_min;
-	return db;
+	return (percentage * 100) + db_min;
 
 func get_property(property: VolumeProperty) -> float:
-	return parse_percentage_to_db(audio_percentages[property]);
+	return parse_percentage_to_db(audio_percentages[property] * global_volume_percentage);
